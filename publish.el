3b2ce146bc6e651df8ac9910d08da05d88c06fb6
Yann Herklotz <git@yannherklotz.com>
2021-09-15 14:08:32 +0100

    Remove org-contrib

--- publish.el
+++ publish.el
@@ -3,7 +3,6 @@
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-refresh-contents)
-(package-install 'org-plus-contrib)
 (package-install 'ox-hugo)
 
 (require 'org)
